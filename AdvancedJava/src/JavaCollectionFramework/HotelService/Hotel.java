e876e96435104c105bbf56b9d40fadb6669bca14
gyeomii <kimsg7388@gmail.com>
2022-05-19 14:13:48 +0900

    HotelService

---
+++ AdvancedJava/src/JavaCollectionFramework/HotelService/Hotel.java
@@ -0,0 +1,37 @@
+package JavaCollectionFramework.HotelService;
+
+import java.util.*;
+
+public class Hotel {
+	public static void main(String[] args) {
+
+	}
+}
+
+class HotelService {
+	Scanner scanner = new Scanner(System.in);
+
+	public void open() {
+
+	}
+
+	public void checkIn() {
+
+	}
+
+	public void checkOut() {
+
+	}
+
+	public void roomInfo() {
+
+	}
+
+	public void close() {
+
+	}
+}
+
+class Room {
+
+}
