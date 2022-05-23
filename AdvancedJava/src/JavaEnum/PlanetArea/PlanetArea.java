352731478b4ae7543791ae0d8820c9603b79d7c6
gyeomii <kimsg7388@gmail.com>
2022-05-23 09:40:36 +0900

    Collections Enum

--- AdvancedJava/src/JavaEnum/PlanetArea/PlanetArea.java
+++ AdvancedJava/src/JavaEnum/PlanetArea/PlanetArea.java
@@ -2,8 +2,9 @@ package JavaEnum.PlanetArea;
 
 public class PlanetArea {
 	public enum Planet {
+		//행성명(반지름km)
 		수성(2439), 금성(6052), 지구(6371), 화성(3390), 목성(69911), 토성(58232), 천왕성(25362), 해왕성(24622);
-
+		
 		private double str;
 
 		Planet(double data) {
@@ -17,9 +18,9 @@ public class PlanetArea {
 
 	public static void main(String[] args) {
 
-		for (Planet pl : Planet.values()) {
-			System.out.printf("%s의 면적 : %f㎢, 부피 : %f㎦\n", pl, (4 * Math.PI * Math.pow(pl.getStr(), 2)), 
-																		 (4 * Math.PI * Math.pow(pl.getStr(), 3)) / 3);
+		for (Planet pl : Planet.values()) { // 면적 : 4πr^2, 부피 : (4/3) * (πr^3)
+			System.out.printf("%s의 면적 : %f㎢, 부피 : %f㎦\n", pl.name(), (4 * Math.PI * Math.pow(pl.getStr(), 2)), 
+																		    (4 * Math.PI * Math.pow(pl.getStr(), 3)) / 3);
 		}
 
 	}
