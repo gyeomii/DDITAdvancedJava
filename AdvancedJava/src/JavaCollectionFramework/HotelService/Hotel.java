a60f923376987e43eafb711fcceb33d54ea8d71a
gyeomii <kimsg7388@gmail.com>
2022-05-23 09:03:01 +0900

    Collections Map

--- AdvancedJava/src/JavaCollectionFramework/HotelService/Hotel.java
+++ AdvancedJava/src/JavaCollectionFramework/HotelService/Hotel.java
@@ -4,19 +4,19 @@ import java.util.*;
 
 public class Hotel {
 	public static void main(String[] args) {
-		new HotelService().open();
+			new HotelService().open();
 	}
 }
 
 class HotelService {
 	private Scanner scanner;
 	private boolean isOpen;
-	private Map<String, Room> roomMap;
+	private TreeMap<Integer, Room> roomMap;
 
 	public HotelService() {
 		scanner = new Scanner(System.in);
 		isOpen = true;
-		roomMap = new TreeMap<String, Room>();
+		roomMap = new TreeMap<Integer, Room>();
 	}
 
 	public void displayMenu() {
@@ -36,10 +36,18 @@ class HotelService {
 		while (isOpen) {
 			switch (menu()) {
 			case 1:
-				checkIn();
+				try {
+					checkIn();
+				} catch (Exception e) {
+					System.err.println("호실을 정수로 입력하세요.");
+				}
 				break;
 			case 2:
-				checkOut();
+				try {
+					checkOut();
+				} catch (Exception e) {
+					System.err.println("호실을 정수로 입력하세요.");
+				}
 				break;
 			case 3:
 				roomInfo();
@@ -48,21 +56,21 @@ class HotelService {
 				close();
 				break;
 			default:
-				System.out.println("메뉴에 있는 번호를 입력하세요.");
+				System.err.println("메뉴에 있는 번호를 입력하세요.");
 
 			}
 		}
 	}
 
-	public void checkIn() {
-		String inNum;
+	public void checkIn() throws Exception{
+		int inNum;
 		System.out.println();
 		System.out.println("몇호실에 체크인 하시겠습니까?");
 		do {
 			System.out.print("실 번호 입력 : ");
-			inNum = scanner.next();
+			inNum = Integer.parseInt(scanner.next());
 			if (roomMap.get(inNum) != null) { // 중복데이터 검사
-				System.out.println(inNum + "호는 사용중입니다.");
+				System.err.println(inNum + "호는 사용중입니다.");
 				System.out.println("다른 호실 번호를 입력해주세요.");
 			}
 		} while (roomMap.get(inNum) != null);
@@ -75,22 +83,22 @@ class HotelService {
 		System.out.println(name + "님 체크인 되었습니다.");
 	}
 
-	public void checkOut() {
+	public void checkOut() throws Exception{
 		System.out.println("몇호실을 체크아웃 하시겠습니까?");
 		System.out.print("실 번호 입력 : ");
-		String outNum = scanner.next();
+		int outNum = Integer.parseInt(scanner.next());
 		Room r = roomMap.get(outNum);
-		String outName = r.getName();
 		if (roomMap.remove(outNum) == null) {
 			System.out.println(outNum + "호실에는 체크인한 사람이 없습니다.");
 			return;
 		} else {
+			String outName = r.getName();
 			System.out.println(outName + "님 체크아웃 되었습니다.");
 		}
 	}
 
 	public void roomInfo() {
-		Set<String> keySet = roomMap.keySet();
+		Set<Integer> keySet = roomMap.keySet();
 
 		if (keySet.size() == 0) {
 			System.out.println("등록된 정보가 없습니다.");
@@ -99,7 +107,7 @@ class HotelService {
 			System.out.println(" 실 번호    이름 ");
 			System.out.println("=============================");
 
-			for (String roomNum : keySet) {
+			for (Integer roomNum : keySet) {
 				Room r = roomMap.get(roomNum);
 				System.out.println("  " + r.getRoomNum() + "\t" + r.getName());
 			}
@@ -121,7 +129,7 @@ class HotelService {
 				int num = scanner.nextInt();
 				return num;
 			} catch (Exception e) { // 오류발생시 재실행
-				System.out.println("Error : 잘못된 값이 입력되었습니다.");
+				System.err.println("Error : 잘못된 값이 입력되었습니다.");
 				scanner.next();
 			}
 		}
@@ -129,20 +137,20 @@ class HotelService {
 }
 
 class Room {
-	private String roomNum;
+	private int roomNum;
 	private String name;
 
-	public Room(String roomNum, String name) {
+	public Room(int inNum, String name) {
 		super();
-		this.roomNum = roomNum;
+		this.roomNum = inNum;
 		this.name = name;
 	}
 
-	public String getRoomNum() {
+	public int getRoomNum() {
 		return roomNum;
 	}
 
-	public void setRoomNum(String roomNum) {
+	public void setRoomNum(int roomNum) {
 		this.roomNum = roomNum;
 	}
 
